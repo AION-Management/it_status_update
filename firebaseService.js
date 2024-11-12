7d5c4149eab8f7bd7bc5ea17cedeb9574f41f6b1
r-agarwal24 <r.agarwal@aionmanagement.com>
2024-11-12 10:02:02 -0500

    Update firebaseService.js

--- firebaseService.js
+++ firebaseService.js
@@ -1,20 +1,16 @@
-import { database } from "./firebaseConfig.js";
-import { ref, set, onValue } from "/firebase/database";
-
-// Function to write data to Firebase
-export function writeUserData(app, status, errormsg) {
-    const dbRef = ref(database, 'users/' + app);
-    return set(dbRef, {
+// firebaseService.js
+function writeUserData(app, status, errormsg) {
+    const dbRef = firebase.database().ref('users/' + app);
+    return dbRef.set({
         app: app,
         status: status,
         errormsg: errormsg
     });
 }
 
-// Function to read data from Firebase
-export function getStatusUpdates(app, callback) {
-    const dbRef = ref(database, 'users/' + app);
-    onValue(dbRef, (snapshot) => {
+function getStatusUpdates(app, callback) {
+    const dbRef = firebase.database().ref('users/' + app);
+    dbRef.on('value', (snapshot) => {
         const data = snapshot.val();
         callback(data);
     });
