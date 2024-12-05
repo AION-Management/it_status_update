dd7e1e68639c98e32622e27a0fb3641ddb315ac3
r-agarwal24 <r.agarwal@aionmanagement.com>
2024-12-05 08:52:36 -0500

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,13 +1,11 @@
 // Your Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAt_UgVnzRq-E-9MrUj02Cyupy_mL1Gfa0",
-  authDomain: "status-update-c5a72.firebaseapp.com",
-  databaseURL: "https://status-update-c5a72-default-rtdb.firebaseio.com",
-  projectId: "status-update-c5a72",
-  storageBucket: "status-update-c5a72.firebasestorage.app",
-  messagingSenderId: "706934655596",
-  appId: "1:706934655596:web:25c622f2d93e6648e72649",
-  measurementId: "G-FF3BKTGF69"
+  apiKey: "AIzaSyBg8do0VhMZqs0gxO0ByVcFmFZGx0FQSss",
+  authDomain: "status-update-1db17.firebaseapp.com",
+  projectId: "status-update-1db17",
+  storageBucket: "status-update-1db17.firebasestorage.app",
+  messagingSenderId: "265169144140",
+  appId: "1:265169144140:web:a5803bb513afb22874e7e9"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
